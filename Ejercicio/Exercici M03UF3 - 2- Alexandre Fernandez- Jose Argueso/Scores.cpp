01965b32eba5c52451e9b7e5bc30e58dcc443ebd
Alexandre Fernandez <alexandre.fernandez@enti.cat>
2025-02-20 13:50:52 +0100

    Cambios finales

--- Ejercicio/Exercici M03UF3 - 2- Alexandre Fernandez- Jose Argueso/Scores.cpp
+++ Ejercicio/Exercici M03UF3 - 2- Alexandre Fernandez- Jose Argueso/Scores.cpp
@@ -2,22 +2,43 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include "Backstage.h"
-
 
 const std::string SCORE_FILE = "scores.wcs";
 
 void saveScore(const std::string& playerName, int score) {
-    std::ofstream file(SCORE_FILE, std::ios::binary | std::ios::app);
+    std::vector<std::pair<std::string, int>> scores = loadScores();
+    bool found = false;
+
+    for (auto& entry : scores) {
+        if (entry.first == playerName) {
+            if (score > entry.second) {
+                entry.second = score;
+                std::cout << "Nueva puntuacion guardada para " << playerName << ".\n";
+            }
+            else {
+                std::cout << "La nueva puntuacion es menor. No se actualizara.\n";
+            }
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        scores.emplace_back(playerName, score);
+    }
+
+    std::ofstream file(SCORE_FILE, std::ios::binary | std::ios::trunc);
     if (!file) {
         std::cerr << "Error al abrir el archivo de puntuaciones.\n";
         return;
     }
 
-    size_t nameLength = playerName.size();
-    file.write(reinterpret_cast<const char*>(&nameLength), sizeof(nameLength));
-    file.write(playerName.c_str(), nameLength);
-    file.write(reinterpret_cast<const char*>(&score), sizeof(score));
+    for (const auto& entry : scores) {
+        size_t nameLength = entry.first.size();
+        file.write(reinterpret_cast<const char*>(&nameLength), sizeof(nameLength));
+        file.write(entry.first.c_str(), nameLength);
+        file.write(reinterpret_cast<const char*>(&entry.second), sizeof(entry.second));
+    }
     file.close();
 }
 
@@ -30,10 +51,7 @@ std::vector<std::pair<std::string, int>> loadScores() {
     }
 
     while (file.peek() != EOF) {
-      
         size_t nameLength;
-
-
         file.read(reinterpret_cast<char*>(&nameLength), sizeof(nameLength));
 
         std::string playerName(nameLength, ' ');
@@ -43,8 +61,6 @@ std::vector<std::pair<std::string, int>> loadScores() {
         file.read(reinterpret_cast<char*>(&score), sizeof(score));
 
         scores.emplace_back(playerName, score);
-  
-
     }
 
     file.close();
@@ -61,11 +77,7 @@ void displayScores() {
     std::cout << "\nHigh Scores:\n";
     for (const auto& entry : scores) {
         std::cout << "Jugador: " << entry.first << " - Puntuacion: " << entry.second << "\n";
-      
-
-  
-   }
-    pause();
+    }
 }
 
 int calculateScore(const std::vector<std::vector<char>>& bottles, int movesLeft) {
@@ -91,3 +103,5 @@ int calculateScore(const std::vector<std::vector<char>>& bottles, int movesLeft)
     score += movesLeft * 3;
     return score;
 }
+
+
